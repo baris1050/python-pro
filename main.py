cbfd3b0969b0bf5c12613c3a124d0167fd397c7a
baris1050 <165493711+baris1050@users.noreply.github.com>
2024-03-30 19:29:21 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+if word=="CRINGE":
+    print("Garip ya da utandırıcı bir şey")
+if word=="LOL":
+    print("Komik bir şeye verilen cevap")
+else:
+    print("tekrar deneyiniz")
